52d6db6f23446144fcb0c5f1aec00984611a1ebe
Cory Francis Myers <cfm@panix.com>
2023-12-25 15:23:14 -0500

    feat: emit one character every 0.1 sec

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use rand::Rng;
+use std::io;
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+
+const WAIT: Duration = Duration::from_millis(100); // 0.1 sec
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let letter: char = rng.gen_range('a'..='z');
-    println!("{}", letter);
+    loop {
+        let mut rng = rand::thread_rng();
+        let letter: char = rng.gen_range('a'..='z');
+
+        print!("{}", letter);
+        io::stdout().flush().unwrap();
+        thread::sleep(WAIT);
+    }
 }
