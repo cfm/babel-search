1180448d0d26b4ebf44c4b57e174a50add47eba5
Cory Francis Myers <cfm@panix.com>
2023-12-25 16:06:15 -0500

    feat: once we have the tip of a needle, continue searching for the rest

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,28 @@ use std::time::Duration;
 const WAIT: Duration = Duration::from_millis(100); // 0.1 sec
 
 fn main() {
-    let needles: Vec<String> = vec!["foo".into(), "bar".into()];
+    let needles: Vec<String> = vec!["foo".into(), "bar".into()]; // TODO: track progress
 
     let mut rng = rand::thread_rng();
-    let mut search = "";
+    let mut search = "".to_string();
     loop {
         let letter: char = rng.gen_range('a'..='z');
-        let mut partial = search.to_owned() + &letter.to_string();
+        let partial = search.to_owned() + &letter.to_string();
         let mut highlight = false;
 
         for needle in &needles {
             if needle.starts_with(&partial) {
                 highlight = true;
+
+                if *needle == partial {
+                    search = "".to_string()
+                } else {
+                    search = partial.clone();
+                }
+                break;
+            } else {
+                highlight = false;
+                search = "".to_string();
             }
         }
 
