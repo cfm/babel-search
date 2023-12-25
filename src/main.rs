27d85d8b48a363da1149e797896c8a6f05e027c0
Cory Francis Myers <cfm@panix.com>
2023-12-25 15:46:09 -0500

    feat: highlight the first character of a found needle

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,21 @@ use std::time::Duration;
 const WAIT: Duration = Duration::from_millis(100); // 0.1 sec
 
 fn main() {
+    let needles: Vec<String> = vec!["foo".into(), "bar".into()];
+
+    let mut rng = rand::thread_rng();
+    let mut search = "";
     loop {
-        let mut rng = rand::thread_rng();
         let letter: char = rng.gen_range('a'..='z');
+        let mut partial = search.to_owned() + &letter.to_string();
+        let mut highlight = false;
+
+        for needle in &needles {
+            if needle.starts_with(&partial) {
+                highlight = true;
+                print!("!");
+            }
+        }
 
         print!("{}", letter);
         io::stdout().flush().unwrap();
